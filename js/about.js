a32782ac9f273aa07377b13d5a3bceb2119511cf
softly320 <122245757+softly320@users.noreply.github.com>
2023-02-08 14:12:43 +0900

    0208
    인덱스 제외 가완성

---
+++ js/about.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    new WOW().init();
+    
+    function skill(id, per, color){
+        let bar = new ProgressBar.Line(id,{
+            strokeWidth: 4,
+            easing: 'easeInOut',
+            duration: 1400,
+            color: '#ffcb05',
+            trailColor: '#ebe8e8',
+            trailWidth: 1,
+            svgStyle: {width: '100%', height: '15px'},
+            from: {color: '#ffcb05'},
+            to: {color: '#f04c23'},
+            step: (state, bar) => {
+            bar.path.setAttribute('stroke', state.color);
+            }
+        })
+        bar.animate(per)
+    }
+    skill("#html", 0.7, "#f04c23");
+    skill("#css",0.7, "#f04c23")
+    skill("#js",0.65, "#f04c23")
+    skill("#ps",0.9, "#f04c23")
+    skill("#ai",0.7, "#f04c23")
+
+    const swiper = new Swiper(".about-work-content",{
+        autoplay:{
+            delay: 1000
+        },
+        loop:true,
+        slidesPerView: 4,
+        spaceBetween: 20,
+        centeredSlides: true,
+        // breakpoints:{
+        //     480: {slidesPerView:1},
+        //     768: {slidesPerView:2},
+        //     1024: {slidesPerView:3},
+        //     1400: {slidesPerView:4}
+        //     // 반응형
+        // },
+        mousewheel:{
+            invert: true
+        }
+    })
+
+
+
+
+
+
+
+
+
+}); //jq
---
+++ js/common.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+    new WOW({
+        animateClass: "animate__animated"
+    }).init()
+
+    
+
+
+    const $fullmenu = $(".fullmenu .full-wrap .left ul li")
+    $fullmenu.mouseover(function(){
+        let i = $(this).index()
+        $fullmenu.removeClass("on").eq(i).addClass("on")
+    }).mouseout(function(){
+        let i = $(this).index()
+        $fullmenu.eq(i).removeClass("on")
+    })
+
+    $(window).scroll(function () {
+        let pos = $(window).scrollTop()
+        console.log(pos)
+        if(pos >= 5){
+            $(".header").addClass("on");
+        }else{
+            $(".header").removeClass("on");
+        }
+    })
+
+    const $nav = $(".header .header-wrap .nav")
+    $nav.click(function(){
+        $(".fullmenu").toggleClass("on")
+        $nav.toggleClass("on")
+        setCSS();
+    })
+
+
+
+    function setCSS() {
+        if ($nav.hasClass("on")){
+            $("html, body").css("overflow", "hidden");
+            $(".header").css("background-color", "transparent")
+        }else{
+            $("html, body").css("overflow", "");
+            $(".header").css("background-color", "")
+        }
+    }
+
+
+
+});
